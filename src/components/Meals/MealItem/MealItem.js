bb8e19f46f4175d94f4da9bd0b794470ba4e3fc2
qp1993 <52257087+qp1993@users.noreply.github.com>
2021-07-12 13:37:23 -0700

    done with cart order, except the err with reduce() method

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/CartContext';
+
+const MealItem = ({ name, description, price, id }) => {
+  const cartCtx = useContext(CartContext);
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: id,
+      name: name,
+      amount: amount,
+      price: price,
+    });
+  };
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{`$${price.toFixed(2)}`}</div>
+      </div>
+      <div>
+        <MealItemForm id={id} onAddToCart={addToCartHandler} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
